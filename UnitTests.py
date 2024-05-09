4e9ff27ba16004dc667876599f070b636faa1c27
msingh0930 <163566286+msingh0930@users.noreply.github.com>
2024-05-09 11:56:17 -0400

    Updated unit tests so that it thoroughly goes through our code.

--- UnitTests.py
+++ UnitTests.py
@@ -1,32 +1,39 @@
 import unittest
-from FinalProject import BudgetManager
-
+import sys
+from FinalProject import BudgetManager, parse_args
+#Unit tests that test each method in our code
 class TestBudgetManager(unittest.TestCase):
     def setUp(self):
-        self.FinalProject = BudgetManager()
-        self.FinalProject.set_income(5000)
+        sys.argv = ['test_script']
+        self.budget_manager = BudgetManager()
+        self.budget_manager.set_income(5000)
 
+    
     def test_set_income(self):
-        self.assertEqual(self.FinalProject.income, 5000)
+        self.assertEqual(self.budget_manager.income, 5000)
 
+    
     def test_transactionlist_add(self):
-        self.FinalProject.transactionlist_add("Groceries", 200)
-        self.FinalProject.transactionlist_add("Dining", 100)
-        self.assertListEqual(self.FinalProject.transactions, [("Groceries", 200), ("Dining", 100)])
+        self.budget_manager.transactionlist_add("Groceries", 200)
+        self.budget_manager.transactionlist_add("Dining", 100)
+        self.assertListEqual(self.budget_manager.transactions, [("Groceries", 200), ("Dining", 100)])
 
+    
     def test_calculate_insights(self):
-        self.FinalProject.transactionlist_add("Groceries", 500)
-        self.FinalProject.transactionlist_add("Rent", 2000)
-        self.FinalProject.transactionlist_add("Entertainment", 300)
-
-        total_amount_spent, total, category_percentages, suggestions = self.FinalProject.calculate_insights()
-
+        self.budget_manager.transactionlist_add("Groceries", 500)
+        self.budget_manager.transactionlist_add("Rent", 2000)
+        self.budget_manager.transactionlist_add("Entertainment", 300)
+        total_amount_spent, total, category_percentages, suggestions = self.budget_manager.calculate_insights()
         self.assertEqual(total_amount_spent, 2800)
         self.assertDictEqual(total, {"Groceries": 500, "Rent": 2000, "Entertainment": 300})
         self.assertDictEqual(category_percentages, {"Groceries": 10.0, "Rent": 40.0, "Entertainment": 6.0})
         self.assertListEqual(suggestions, ["You should consider reducing spending on Rent."])
 
-
+    
+    def test_command_line_args(self):
+        sys.argv = ['test_script', '4500']
+        args = parse_args()
+        self.assertEqual(args.income, 4500)
 
 if __name__ == '__main__':
     unittest.main()
